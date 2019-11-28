b29064a5b1c284f4f9a38b2519c4777401572ac0
github_username <azizkoura@hotmail.com>
2019-11-28 19:49:35 +0100

    loop

--- js-sandbox/app.js
+++ js-sandbox/app.js
@@ -1,52 +1,25 @@
-const id = '100';
+const array = [
+  'carrot',
+  'potatoes',
+  'orange',
+  'kiwi',
+  'pinnaple',
+  'banana',
+  'apple',
+  'pear',
+  'ciron',
+  'crawberry'
+];
 
-if (typeof id === 'undefined') {
-  console.log('correct');
-} else {
-  console.log(`id: ${id}`);
+for (let i = 0; i < array.length; i++) {
+  const arr = array[i];
+  if (arr === 'banana') {
+    console.log(`${arr}! this is my favorite!`);
+    continue;
+  }
+  if (arr === 'apple') {
+    console.log('break ...');
+    break;
+  }
+  console.log(arr);
 }
-
-const name = 'steve';
-const age = 66;
-
-if (age <= 8) {
-  console.log(`${name} is a child`);
-} else if (age > 8 && age <= 18) {
-  console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is adult`);
-}
-
-if (age < 16 || age > 65) {
-  console.log(`${name} can not be registrated`);
-} else {
-  console.log('please registrated');
-}
-
-let day;
-
-switch (new Date().getDay()) {
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 2:
-    day = 'Tuesday';
-    break;
-  case 3:
-    day = 'Wednesday';
-    break;
-  case 4:
-    day = 'Thursday';
-    break;
-  case 5:
-    day = 'Friday';
-    break;
-  case 6:
-    day = 'Saturday';
-    break;
-}
-
-console.log(`you run on ${day}`);
